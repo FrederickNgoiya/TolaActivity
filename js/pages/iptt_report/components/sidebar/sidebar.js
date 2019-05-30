984e84a375f51314f34c854d26a8e2ec11367042
Paul Souders <paul@axoplasm.com>
2019-05-29 18:54:33 -0700

    standardize IPTT sidebar toggle with admin lite

--- js/pages/iptt_report/components/sidebar/sidebar.js
+++ js/pages/iptt_report/components/sidebar/sidebar.js
@@ -3,17 +3,16 @@ import IPTTFilterForm from './filterForm';
 
 const IPTTSidebar = () => {
     return (
-        <div className="sidebar_wrapper">
-            <div className="collapse width show" id="sidebar">
+        <div className="folding-sidebar">
+            <div className="collapse width show folding-sidebar__contents" id="sidebar">
                 <IPTTFilterForm />
             </div>
-            <div className="sidebar-toggle">
-              <a href="#" data-target="#sidebar" data-toggle="collapse"
+            <div className="folding-sidebar__trigger"
+                data-target="#sidebar" data-toggle="collapse"
                     title={
                         /* # Translators: A toggle button that hides a sidebar of filter options */
                         gettext('Show/Hide Filters') }>
                 <i className="fa fa-chevron-left"></i>
-              </a>
             </div>
         </div>
     );
